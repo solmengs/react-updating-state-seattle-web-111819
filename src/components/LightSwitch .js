6796b579feee61a9620ce80f477c5ca4bb978920
github username <github email address>
2020-01-28 11:06:01 -0800

    Done.

---
+++ src/components/LightSwitch .js
@@ -0,0 +1,38 @@
+// Code ClickityClick Component Here
+import React from 'react'
+
+
+class LightSwitch extends React.Component {
+  constructor() {
+    super();
+
+    // Initial state is defined
+    this.state = {
+      toggled: false
+    };
+  }
+
+  // when handleClick is called, setState will update the state so that toggle is reversed
+  // handleClick = () => {
+  //   this.setState(previousState => {
+  //     return {
+  //       toggled: !previousState.toggled
+  //     }
+  //   })
+  // }
+handleClick = () => {
+  let newval = this.state.toggled
+  this.setState({
+    toggled: !newval
+  })
+}
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>{this.state.toggled ? "ON" : "OFF"}</button>
+      </div>
+    );
+  }
+}
+
+export default LightSwitch;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ClickityClick from './components/LightSwitch ';
+import LightSwitch from './components/LightSwitch ';
+ReactDOM.render(<ClickityClick />, document.getElementById('root'));
 
 ReactDOM.render(
   <div>
     Mount Components Here
+    <LightSwitch />
   </div>,
   document.getElementById('root')
 );
